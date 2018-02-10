2fbfbfe5921dc5fcc1a1f45f9a35040bc8bfa0b0
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-02-10 07:25:02 +0000

    removing unncessary css

--- app/pages/dmnpage/components/DMNPage.jsx
+++ app/pages/dmnpage/components/DMNPage.jsx
@@ -3,8 +3,15 @@ import React from "react";
 class DMNPage extends React.Component {
 
     render() {
-        return <div className="center">
-            Not yet implemented.
+        return <div>
+            <div className="content" name="js-drop-zone" id="js-drop-zone"
+                 style={{height: '90%', position: 'absolute'}}>
+                <div className="message intro">
+                    <div className="note">
+                        Not implemented yet
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
