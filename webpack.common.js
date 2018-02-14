67ebba1655c605aaa62ecd4718daaa7fe38b45c4
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-02-14 08:23:29 +0000

    adding additional plugin

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const sourcePath = path.join(__dirname, './app');
 const buildDirectory = path.join(__dirname, './dist');
-
+const SriPlugin = require('webpack-subresource-integrity');
 
 module.exports = {
     resolve: {
@@ -16,8 +16,13 @@ module.exports = {
         path: buildDirectory,
         filename: 'bundle.js',
         publicPath: '/',
+        crossOriginLoading: 'anonymous',
     },
     plugins: [
+        new SriPlugin({
+            hashFuncNames: ['sha384'],
+            enabled: process.env.NODE_ENV === 'production',
+        }),
         new webpack.NamedModulesPlugin(),
         new HtmlWebpackPlugin({
             template: './public/index.html',
